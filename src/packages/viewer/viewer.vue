5f14d3564a0bf166adaf88a722524ce328b5f5a0
Jeff Gueldre <jfgueldre@hotmail.com>
2020-07-17 10:53:43 +0200

    lerna test 3

--- src/packages/viewer/viewer.vue
+++ src/packages/viewer/viewer.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>{{ msg }}</div>
+  <div>
+    {{ msg }}
+    <div>
+      <test test="Working!"></test>
+    </div>
+  </div>
 </template>
 <script>
+import test from "../../components/test";
 export default {
+  components: {
+    test,
+  },
   data: function () {
     return {};
   },
